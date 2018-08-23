e16d7c6cfd594f962c321193c1d36aa93eaa0098
PatrickYuanMingcan <31325100+PatrickYuanMingcan@users.noreply.github.com>
2018-08-23 19:58:42 -0400

    Update test_tree.py

--- HW2/tests/test_tree.py
+++ HW2/tests/test_tree.py
@@ -7,27 +7,26 @@ Created on Thu Aug 23 10:07:34 2018
 
 import unittest
 from tree.add_tree import Tree
-from tree.add_tree import Node
 
 class TestTree(unittest.TestCase):
     
     def test1(self):
-        t = Tree(Node(1, None, None))
+        t = Tree(1)
         assert t.printTree() == [['1']]
         
     def test2(self):
-        t = Tree(Node(1, None, None))
-        t.left = Tree(Node(2, None, None))
-        t.right = Tree(Node(3, None, None))
-        t.left.left = Tree(Node(4, None, None))
+        t = Tree(1)
+        t.left = Tree(2)
+        t.right = Tree(3)
+        t.left.left = Tree(4)
         assert t.printTree() == [['|','|','|','1','|','|','|'],
                           ['|','2','|','|','|','3','|'],
                           ['4','|','|','|','|','|','|',]]
         
     def test3(self):
-        t = Tree(Node(1, None, None))
-        t.left = Tree(Node(2, None, None))
-        t.left.left = Tree(Node(3, None, None))
+        t = Tree(1)
+        t.left = Tree(2)
+        t.left.left = Tree(3)
         assert t.printTree() == [['|','|','|','1','|','|','|'],
                           ['|','2','|','|','|','|','|'],
                           ['3','|','|','|','|','|','|',]]
