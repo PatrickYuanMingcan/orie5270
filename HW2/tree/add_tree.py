3c95237b5d3e50e3a4c40be60921a3782157d6a1
PatrickYuanMingcan <31325100+PatrickYuanMingcan@users.noreply.github.com>
2018-08-23 19:59:12 -0400

    Update add_tree.py

--- HW2/tree/add_tree.py
+++ HW2/tree/add_tree.py
@@ -6,25 +6,20 @@ Created on Thu Aug 23 10:02:24 2018
 """
 
 class Tree(object):
-    def __init__(self, root):
-       self.root = root
-       
-    def get_value_root(self):
-       if self.root is not None:
-           return self.root.value
-       else:
-           return None
+    def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
        
     def printTree(self):
-        root = self.root
         def get_level(node):
             if not node:
                 return 0
             return 1 + max(get_level(node.right), get_level(node.left))
-        m = get_level(root)
+        m = get_level(self)
         n = 2**m - 1
         ans = [['|' for i in range(n)] for j in range(m)]
-        def dfs(node = root, level = 0, pos = 0):
+        def dfs(node = self, level = 0, pos = 0):
             if not node:
                 return
             i = 2**(m - level - 1) - 1 + pos*(2**(m - level))
@@ -34,16 +29,8 @@ class Tree(object):
         dfs()
         return ans
 
-
-class Node(object):
-
-   def __init__(self, value, left, right):
-       self.value = value
-       self.left = left
-       self.right = right
-
-
 if __name__ == '__main__':
-   a = Node(2, None, None)
-   b = Tree(a)
-   print(b.get_value_root())
+   t = Tree(1)
+   t.left = Tree(2)
+   t.left.left = Tree(3)
+   print(t.printTree())
