bd0d22d6b3a2eb7d16b9d4dfca5621b66aa883fa
PatrickYuanMingcan <31325100+PatrickYuanMingcan@users.noreply.github.com>
2018-08-23 20:45:07 -0400

    Update test_tree.py

--- HW2/tests/test_tree.py
+++ HW2/tests/test_tree.py
@@ -5,10 +5,13 @@ Created on Thu Aug 23 10:07:34 2018
 """
 import unittest
 from tree.add_tree import Tree
+
+
 class TestTree(unittest.TestCase):
     def test1(self):
         t = Tree(1)
-        assert t.printTree() == [['1']] 
+        assert t.printTree() == [['1']]
+
     def test2(self):
         t = Tree(1)
         t.left = Tree(2)
@@ -17,6 +20,7 @@ class TestTree(unittest.TestCase):
         assert t.printTree() == [['|', '|', '|', '1', '|', '|', '|'],
                           ['|', '2', '|', '|', '|', '3', '|'], 
                           ['4', '|', '|', '|', '|', '|', '|']]
+
     def test3(self):
         t = Tree(1)
         t.left = Tree(2)
